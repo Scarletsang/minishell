6be0591b9a1e0254562ff922c5b599ff6a3f64d4
Anthony Tsang <htsang@student.42heilbronn.de>
2023-05-07 23:03:27 +0200

    chore(main): add a very basic setup for minishell

--- src/main.c
+++ src/main.c
@@ -6,13 +6,32 @@
 /*   By: htsang <htsang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/01 22:20:29 by htsang            #+#    #+#             */
-/*   Updated: 2023/04/23 23:34:42 by htsang           ###   ########.fr       */
+/*   Updated: 2023/05/07 22:31:41 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "MINISHELL/minishell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
-int	main(int argc, const char **argv)
+int main(void)
 {
-	return (EXIT_SUCCESS);
+	char	*line;
+
+	line = readline("minishell>");
+	while (line) {
+		// TODO: Parsing + lexing
+		// TODO: expander + execution here
+		printf("You entered: %s\n", line);
+		add_history(line);
+		 // Clear the current input line
+		rl_replace_line("", 0);
+		// Update the display of the input line
+		rl_redisplay();
+		// Free the memory allocated by readline
+		free(line);
+		line = readline("minishell>");
+	}
+	return 0;
 }
