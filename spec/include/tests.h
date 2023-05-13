418a823123d14ea0c40613cadd4265fd4ad6f947
Anthony Tsang <anthonytsang0226@gmail.com>
2023-05-13 09:17:01 +0200

    test: hashtable

--- spec/include/tests.h
+++ spec/include/tests.h
@@ -2,5 +2,7 @@
 # define TESTS_H
 
 void	expander_hashtable_test1();
+void	expander_hashtable_test2();
+void	expander_hashtable_test3();
 
 #endif
--- spec/tests/expander.c
+++ spec/tests/expander.c
@@ -6,7 +6,7 @@
 /*   By: anthonytsang <anthonytsang@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 01:45:33 by anthonytsan       #+#    #+#             */
-/*   Updated: 2023/05/13 04:38:59 by anthonytsan      ###   ########.fr       */
+/*   Updated: 2023/05/13 08:06:41 by anthonytsan      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ void	expander_hashtable_test1()
 	ht_add(&ht, ft_strdup("3key"), ft_strdup("3value"));
 	ht_add(&ht, ft_strdup("4key"), ft_strdup("4value"));
 	ht_add(&ht, ft_strdup("5key"), ft_strdup("5value"));
+	printf("\n");
 	ht_print(&ht);
 	printf("got: %s\n", (char *) ht_get(&ht, "key1"));
 	printf("got: %s\n", (char *) ht_get(&ht, "key2"));
@@ -35,5 +36,74 @@ void	expander_hashtable_test1()
 	printf("got: %s\n", (char *) ht_get(&ht, "3key"));
 	printf("got: %s\n", (char *) ht_get(&ht, "4key"));
 	printf("got: %s\n", (char *) ht_get(&ht, "5key"));
+	printf("\n");
+	ht_del(&ht, "key1");
+	ht_del(&ht, "key2");
+	ht_del(&ht, "key3");
+	ht_del(&ht, "2key");
+	ht_del(&ht, "3key");
+	ht_del(&ht, "4key");
+	printf("got: %s\n", (char *) ht_get(&ht, "key1"));
+	printf("got: %s\n", (char *) ht_get(&ht, "key2"));
+	printf("got: %s\n", (char *) ht_get(&ht, "key3"));
+	printf("got: %s\n", (char *) ht_get(&ht, "1key"));
+	printf("got: %s\n", (char *) ht_get(&ht, "2key"));
+	printf("jgot: %s\n", (char *) ht_get(&ht, "3key"));
+	printf("got: %s\n", (char *) ht_get(&ht, "4key"));
+	printf("got: %s\n", (char *) ht_get(&ht, "5key"));
+	printf("\n");
+	ht_print(&ht);
+	ht_destroy(&ht);
+}
+
+void	expander_hashtable_test2()
+{
+	struct s_ht	ht;
+
+	ht_create(&ht, 20);
+	ht_add(&ht, ft_strdup("3key"), ft_strdup("3value"));
+	ht_del(&ht, "3key");
+	ht_print(&ht);
+}
+
+void	expander_hashtable_test3()
+{
+	struct s_ht	ht;
+	unsigned char	key;
+	unsigned char	total;
+	unsigned char	key_start;
+
+	key_start = 'a';
+	total = 100;
+	ht_create(&ht, 30);
+	key = key_start;
+	while (key < (key_start + total))
+	{
+		ht_add(&ht, ft_strdup((char[2]) {key, 0}), ft_strdup((char[3]) {key, '!', 0}));
+		key++;
+	}
+	ht_print(&ht);
+	key = key_start;
+	while (key < (key_start + total))
+	{
+		printf("got: %s\n", (char *) ht_get(&ht, (char[2]) {key, 0}));
+		key++;
+	}
+	printf("\n");
+	ht_print(&ht);
+	key = key_start;
+	while (key < (key_start + (total / 2)))
+	{
+		ht_del(&ht, (char[2]) {key, 0});
+		key++;
+	}
+	key = key_start;
+	while (key < (key_start + total))
+	{
+		printf("got: %s\n", (char *) ht_get(&ht, (char[2]) {key, 0}));
+		key++;
+	}
+	printf("\n");
+	ht_print(&ht);
 	ht_destroy(&ht);
 }
