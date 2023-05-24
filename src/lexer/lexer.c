0f22dfeb1a5a6d40abdeea137fff3dca7d23af95
Sangyao42 <wangsangyao@hotmail.com>
2023-05-24 21:44:33 +0200

    Updated: grammer & lexer 3rd

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 13:11:34 by sawang            #+#    #+#             */
-/*   Updated: 2023/05/23 22:40:36 by sawang           ###   ########.fr       */
+/*   Updated: 2023/05/24 13:47:21 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,71 +25,75 @@ static bool	scanner_at_end(struct s_scanner scanner)
 	return (*scanner.current == '\0');
 }
 
-// void	tokens_create(struct s_lexer *lexer)
-// {
-// 	char			c;
-// 	struct s_token	token;
+static char	scanner_peek(struct s_scanner scanner)
+{
+	return (*scanner.current);
+}
 
-// 	while (1)
-// 	{
-// 		token = token_scan(lexer);
-// 		if (token.type == TOKEN_EOF)
-// 			break ;
-// 		token_add_one(lexer, token);
-// 	}
-// };
-
-// token	*idk(char *str)
-// {
-// 	if(match(lexer, ">>"))
-// 		return (makeToken(str);
-// 	if(match(lexer, ">"))
-// 		return (makeToken(str);
-// 	if ()
-// 	return (NULL);
-// }
 static char	scanner_advance(struct s_scanner *scanner)
 {
 	scanner->current++;
 	return (scanner->current[-1]);
 }
 
-struct s_token token_make(t_token_type type, struct s_scanner scanner, int length)
+struct s_token	token_make(t_token_type type, struct s_scanner scanner)
 {
 	struct s_token	token;
 
 	token.type = type;
 	token.start = scanner.start;
-	token.length = length;
+	token.length = scanner.current - scanner.start;
 	return (token);
 }
 
-t_token_type	match(char expected, char previous, int *scanned)
+struct s_token	token_error(const char *message)
 {
-	if (expected == '\0')
+	struct s_token	token;
+
+	token.type = TOKEN_ERROR;
+	token.start = message;
+	token.length = ft_strlen(message);
+	return (token);
+}
+
+struct s_token	token_match_operator(struct s_scanner *scanner, char c)
+{
+	if (c == '>')
 	{
-		if (previous == '>')
-			return (token_make(TOKEN_GREAT, lexer->scanner, 1));
-		else if (previous == '<')
-			return (token_make(TOKEN_LESS, lexer->scanner, 1));
+		if (*scanner->current == '>')
+		{
+			scanner_advance(scanner);
+			return (token_make(TOKEN_DGREAT, *scanner));
+		}
+		else
+			return (token_make(TOKEN_GREAT, *scanner));
 	}
-	if (expected == previous)
+	if (c == '<')
 	{
-		*scanned = 2;
-		if (str[0] == '>')
-			return (token_make(TOKEN_DGREAT, lexer->scanner, 2));
-		else if (str[0] == '<')
-			return (token_make(TOKEN_DLESS, lexer->scanner, 2));
+		if (*scanner->current == '<')
+		{
+			scanner_advance(scanner);
+			return (token_make(TOKEN_DLESS, *scanner));
+		}
+		else
+			return (token_make(TOKEN_LESS, *scanner));
 	}
-	else
-	{
-		*scanned = 1;
-		if (str[0] == '>')
-			return (token_make(TOKEN_GREAT, lexer->scanner, 1));
-		else if (str[0] == '<')
-			return (token_make(TOKEN_LESS, lexer->scanner, 1));
-	}
-	return (token_err("Unexpected character.\n"));
+	return (token_error("Unexpected character.\n"));
+}
+
+struct s_token	token_match_word(struct s_scanner *scanner, char c)
+{
+	while (scanner_peek(*scanner) != c && !scanner_at_end(*scanner))
+		scanner_advance(scanner);
+	if (scanner_at_end(*scanner))
+		return (token_error("Unterminated quote.\n"));
+	scanner_advance(scanner);
+	return (token_make(TOKEN_WORD, *scanner));
+}
+
+struct s_token	token_match_word_or_assignmentword(struct s_scanner *scanner)
+{
+
 }
 
 struct s_token	token_scan(struct s_lexer *lexer)
@@ -101,23 +105,19 @@ struct s_token	token_scan(struct s_lexer *lexer)
 	scanner_skipwhitespace(&lexer->scanner);
 	lexer->scanner.start = lexer->scanner.current;
 	if (scanner_at_end(lexer->scanner))
-		return (token_make(TOKEN_EOF, lexer->scanner, 0));
+		return (token_make(TOKEN_EOF, lexer->scanner));
 	c = scanner_advance(&lexer->scanner);
 	if (c == '|')
-		return (token_make(TOKEN_PIPE, lexer->scanner, 1));
-	// if (c == '\'')
-	// 	return (token_make(TOKEN_QUOTE, lexer->scanner, 1));
-	// if (c == '\"')
-	// 	return (token_make(TOKEN_DQUOTE, lexer->scanner, 1));
-	if (c == '>')
-		return (match_operator(lexer->scanner, scanned));
-	if (c == '<')
-		return (token_make(match(lexer->scanner.current, c, &scanned), lexer->scanner, scanned));
+		return (token_make(TOKEN_PIPE, lexer->scanner));
+	if (c == '>' || c == '<')
+		return (token_match_operator(&lexer->scanner, c));
+	// if (c == '<')
+	// 	return (match_operator(lexer->scanner, c));
 	if (c == '\'' || c == '\"')
-		return (match_word(lexer->scanner, &scanned));
+		return (token_match_word(&lexer->scanner, c));
 	else
-		return (match_word_or_assignmentword(lexer->scanner, &scanned));
-	return (token_err("Unexpected character.\n"));
+		return (token_match_word_or_assignmentword(lexer->scanner));
+	return (token_error("Unexpected character.\n"));
 }
 
 struct s_token_list	*token_list_get(char *line)
