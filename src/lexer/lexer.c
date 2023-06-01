69c37bdd4220f184372037f6d9e2959c20ed6a9d
Sangyao42 <wangsangyao@hotmail.com>
2023-06-01 15:04:01 +0200

    Updated: lexer, saving TOKEN_ERROR and TOK_EOF in linked list

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,12 +6,11 @@
 /*   By: sawang <sawang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 13:11:34 by sawang            #+#    #+#             */
-/*   Updated: 2023/05/31 22:23:34 by sawang           ###   ########.fr       */
+/*   Updated: 2023/06/01 14:53:31 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MINISHELL/lexer/lexer.h"
-#include <stdio.h>
 
 void	lexer_init(struct s_lexer *lexer)
 {
@@ -21,7 +20,7 @@ void	lexer_init(struct s_lexer *lexer)
 	// lexer->scanner.current = line;
 }
 
-t_err_code	token_list_get(struct s_lexer *lexer, char *line)
+t_exit_code	token_list_get(struct s_lexer *lexer, char *line)
 {
 	struct s_scanner	scanner;
 	struct s_token		token;
@@ -33,48 +32,22 @@ t_err_code	token_list_get(struct s_lexer *lexer, char *line)
 	while (1)
 	{
 		token = token_scan(&scanner);
-		if (token.type == TOKEN_ERROR)
-			return (token_clear_when_lexer_failed(lexer, \
-			(t_token_cleaner)del, token.start), ERROR_WHEN_LEX);
-		if (token.type == TOKEN_EOF)
-			break ;
+		// if (token.type == TOKEN_ERROR)
+		// 	return (token_clear_when_lexer_failed(lexer, \
+		// 	(t_token_cleaner)del, token.start), ERROR_WHEN_LEX);
+		// if (token.type == TOKEN_EOF)
+		// 	break ;
 		token_lst = token_new(token);
 		if (!token_lst)
 			return (token_clear_when_lexer_failed(lexer, \
 			(t_token_cleaner)del, "malloc failed"), MALLOC_FAIL);
 		token_add_back(lexer, token_lst);
+		if (token.type == TOKEN_ERROR)
+			return (printf("%s\n", token.start), ERROR_WHEN_LEX);
+		if (token.type == TOKEN_EOF)
+		 	break ;
 	}
 	token_lstitr_update_assignmentword(lexer->start, \
 		(t_token_updater)token_update);
 	return (LEXER_SUCCESS);
-}
-
-void	token_update(struct s_token *token)
-{
-	token->type = TOKEN_ASSIGNMENT_WORD;
-}
-
-void	token_lstitr_update_assignmentword(struct s_token_list *token_lst, \
-t_token_updater token_update)
-{
-	struct s_token_list	*tmp_token_node;
-
-	if (!token_lst || !token_update)
-		return ;
-	tmp_token_node = token_lst;
-	while (tmp_token_node)
-	{
-		if (token_is_assignmentword(tmp_token_node->token))
-			token_update(&tmp_token_node->token);
-		tmp_token_node = tmp_token_node->next;
-	}
-}
-
-bool	token_is_assignmentword(struct s_token token)
-{
-	char	*token_str;
-
-	token_str = ft_substr(token.start, 0, token.length);
-
-	free(token_str);
 }
