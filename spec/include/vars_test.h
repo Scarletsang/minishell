94fc84de38047cb52b29e44e53ecca44f8f8e9ba
Anthony Tsang <htsang@student.42heilbronn.de>
2023-06-07 21:42:25 +0200

    spec: Modify shells to adhere to new changes in execution

--- spec/include/vars_test.h
+++ spec/include/vars_test.h
@@ -4,8 +4,8 @@
 # include "tshell.h"
 # include "MINISHELL/execution/vars.h"
 
-struct s_minishell_vars	*vars_shell_init(void);
+struct s_ms_vars	*vars_shell_init(void);
 
-t_tshell_status			vars_shell(struct s_minishell_vars *vars, struct s_tparser *tparser);
+t_tshell_status			vars_shell(struct s_ms_vars *vars, struct s_tparser *tparser);
 
 #endif
