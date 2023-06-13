0ba679aacb732e632e242ba145e48d9b32a1c980
Sangyao42 <wangsangyao@hotmail.com>
2023-06-13 21:42:16 +0200

    Added: parser printer

--- src/parser/parser_tester.c
+++ src/parser/parser_tester.c
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 13:16:51 by sawang            #+#    #+#             */
-/*   Updated: 2023/06/13 15:26:52 by sawang           ###   ########.fr       */
+/*   Updated: 2023/06/13 21:39:27 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,92 @@
 #include "MINISHELL/parser/parser.h"
 #include <readline/readline.h>
 #include <readline/history.h>
+
+typedef enum e_printing_branch {
+	LEFT,
+	RIGHT
+}			t_printing_branch;
+
+struct s_ast_node	*from_identidier_to_tree(struct s_ast_node *node, \
+t_printing_branch branch)
+{
+	if (branch == LEFT)
+		return (node->left);
+	else
+		return (node->right);
+}
+
+void	print_content_redirection(struct s_ast_redirection_vector *redirection)
+{
+	struct s_vector_iterator	*vec_itr;
+
+	vector_iterator_init(vec_itr, (t_vector *)redirection);
+	while (!vector_iterator_end(vec_itr))
+	{
+		printf("type:%d ", \
+		((struct s_ast_redirection *)vector_iterator_current(vec_itr))->type);
+		printf("content:%s ", \
+		((struct s_ast_redirection *)vector_iterator_current(vec_itr))->content);
+		vector_iterator_next(vec_itr);
+	}
+}
+
+void	print_content_sb_vector(t_sb_vector *sb_vector)
+{
+	struct s_vector_iterator	*vec_itr;
+
+	vector_iterator_init(vec_itr, (t_vector *)sb_vector);
+	while (!vector_iterator_end(vec_itr))
+	{
+		printf("%s ", (char *)vector_iterator_current(vec_itr));
+		vector_iterator_next(vec_itr);
+	}
+}
+
+void	print_ast_content(struct s_ast_node_content *content)
+{
+	if (content == NULL)
+		return ;
+	printf("redirection_in: ");
+	print_content_redirection(&content->redirection_in);
+	printf("\tredirection_out: ");
+	print_content_redirection(&content->redirection_out);
+	printf("\tassignment_word: ");
+	print_content_sb_vector(&content->assignment_word);
+	printf("\tcommand: ");
+	print_content_sb_vector(&content->command);
+}
+
+struct s_ast_node	*from_identidier_to_tree(struct s_ast_node *node, t_printing_branch branch)
+{
+	if (branch == LEFT)
+		return (node->left);
+	else
+		return (node->right);
+}
+
+void	print_ast_recursive(struct s_ast_node *node, int level, t_printing_branch branch)
+{
+	if (node == NULL)
+		return ;
+
+	// Print the right subtree
+	print_tree_recursive(from_identidier_to_tree(node, branch), level + 1, branch);
+	// Print the current node
+	for (int i = 0; i < level; i++) {
+		printf("    .");
+    }
+	printf("type: %d\t", node->type);
+	print_ast_content(node->content);
+	// Print the left subtree
+	print_tree_recursive(from_identidier_to_tree(node, !branch), level + ((node->type != AST_NODE_CMD)), branch);
+}
+
+void	print_ast(struct s_ast_node *root)
+{
+    print_tree_recursive(root, 0, LEFT);
+    printf("--------\n");
+}
 
 int	main(void)
 {
@@ -43,7 +129,7 @@ int	main(void)
 				// print syntex error
 				// and free ast
 			else
-				// print ast
+				print_ast(parser.head);
 			// free lexer
 		}
 		else
