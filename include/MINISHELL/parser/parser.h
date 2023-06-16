8fe3e15d7cf0d88f8ebf1d9db575c31e38dcf119
Sangyao42 <wangsangyao@hotmail.com>
2023-06-16 21:25:46 +0200

    Updated: parser.h for updating ast

--- include/MINISHELL/parser/parser.h
+++ include/MINISHELL/parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 21:16:43 by htsang            #+#    #+#             */
-/*   Updated: 2023/06/14 22:00:55 by sawang           ###   ########.fr       */
+/*   Updated: 2023/06/16 21:16:03 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define PARSER_H
 
 # include "MINISHELL/parser/ast.h"
-# include "MINISHELL/lexer/lexer.h"
+# include "MINISHELL/parser/content.h"
 
 //if there is syntax error or malloc fail, free and exit right away
 //PARSER_FAILURE means nothing to parse,
@@ -23,7 +23,6 @@ typedef enum e_parser_exit_code
 {
 	PARSER_SUCCESS,
 	PARSER_FAILURE,
-	// MALLOC_FAIL
 }			t_parser_exit_code;
 
 struct s_parser
@@ -31,9 +30,13 @@ struct s_parser
 	struct s_ast_node	*head;
 	struct s_ast_node	*current;
 	struct s_token_list	*current_token;
-	// struct s_token_list *start_token;
 	bool				malloc_fail;
 };
+
+bool	parser_check_before_run(struct s_lexer *lexer, \
+	t_lexer_exit_code lexer_exit_code);
+void	parser_init(struct s_parser *parser, struct s_token_list *start);
+void	parser_free(struct s_parser *parser);
 
 // TODO: interface for parser
 t_parser_exit_code	parse_io_redirect(struct s_parser *parser);
@@ -47,7 +50,11 @@ t_parser_exit_code	parse_pipe(struct s_parser *parser);
 t_parser_exit_code	parse_eof(struct s_parser *parser);
 t_parser_exit_code	parse_complete_command(struct s_parser *parser);
 
-//free function for parser
+// update ast
+void	parser_ast_cmd_insert(struct s_parser *parser, \
+	struct s_ast_node *new_node);
+void	parser_ast_pipe_insert(struct s_parser *parser, \
+	struct s_ast_node *new_node);
 
 //parser token scanner utils
 void				parser_token_advance(struct s_parser *parser);
