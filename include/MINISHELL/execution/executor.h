46f94f98c0c2b5b0780f980606603b41825dd978
Anthony Tsang <anthonytsang0226@gmail.com>
2023-06-17 18:14:09 +0200

    feat(executor): Save a duplicated fd of stdin for heredoc

--- include/MINISHELL/execution/executor.h
+++ include/MINISHELL/execution/executor.h
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 19:18:03 by htsang            #+#    #+#             */
-/*   Updated: 2023/06/16 20:57:28 by htsang           ###   ########.fr       */
+/*   Updated: 2023/06/17 18:06:06 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,10 @@
 # include "MINISHELL/parser/ast.h"
 # include "MINISHELL/execution/expander.h"
 # include "MINISHELL/execution/executor/piper.h"
+
+# ifndef HEREDOC_FILENAME
+#  define HEREDOC_FILENAME "./.minishell_heredoc.tmp"
+# endif
 
 typedef enum s_executor_exit_code
 {
@@ -37,15 +41,30 @@ struct s_ms_executor
 {
 	struct s_ms_piper	piper;
 	t_vector			envp;
+	int					stdin_fd;
+	int					heredoc_fd;
+	bool				heredoc_fd_opened;
 	pid_t				last_child_pid;
 };
 
 t_executor_exit_code	ms_executor_init(struct s_ms_executor *executor);
 
-t_executor_return_value	ms_executor_wait(struct s_ms_executor *executor);
+t_executor_exit_code	ms_executor_close_heredoc_fd(\
+struct s_ms_executor *executor);
 
 t_executor_exit_code	ms_executor_destroy(struct s_ms_executor *executor);
 
+//////////////////////////////////////////////////////////
+////////////   interface used by ms_execute   ////////////
+//////////////////////////////////////////////////////////
+
+pid_t					ms_executor_fork(struct s_ms_executor *executor);
+
+t_executor_return_value	ms_executor_wait(struct s_ms_executor *executor);
+
+t_executor_exit_code	ms_executor_read_from_fd(int fd);
+
+t_executor_exit_code	ms_executor_write_to_fd(int fd);
 
 ////////////////////////////////////////////
 ////////////   envp interface   ////////////
@@ -66,15 +85,5 @@ t_executor_exit_code	ms_executor_envp_import_from_environment(\
 struct s_ms_executor *executor, struct s_ms_vars *vars);
 
 void					ms_executor_envp_reset(struct s_ms_executor *executor);
-
-//////////////////////////////////////////////////////////
-////////////   interface used by ms_execute   ////////////
-//////////////////////////////////////////////////////////
-
-pid_t					ms_executor_fork(struct s_ms_executor *executor);
-
-t_executor_exit_code	ms_executor_read_from_fd(int fd);
-
-t_executor_exit_code	ms_executor_write_to_fd(int fd);
 
 #endif
--- src/execution/executor/executor.c
+++ src/execution/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 20:00:51 by htsang            #+#    #+#             */
-/*   Updated: 2023/06/16 16:40:13 by htsang           ###   ########.fr       */
+/*   Updated: 2023/06/17 18:05:18 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,46 @@ t_executor_exit_code	ms_executor_init(struct s_ms_executor *executor)
 {
 	if (vector_init(&executor->envp, sizeof(char *), 20, vector_set_string))
 		return (EXECUTION_ERROR);
+	executor->stdin_fd = dup(STDIN_FILENO);
+	if (executor->stdin_fd == -1)
+	{
+		vector_free(&executor->envp);
+		return (EXECUTION_ERROR);
+	}
 	vector_append(&executor->envp, NULL);
 	ms_piper_init(&executor->piper);
+	executor->heredoc_fd_opened = false;
 	executor->last_child_pid = -1;
 	return (EXECUTION_SUCCESS);
 }
 
+t_executor_exit_code	ms_executor_close_heredoc_fd(\
+struct s_ms_executor *executor)
+{
+	t_executor_exit_code	exit_code;
+
+	exit_code = EXECUTION_SUCCESS;
+	if (executor->heredoc_fd_opened)
+	{
+		if (unlink(HEREDOC_FILENAME) == -1)
+			exit_code = EXECUTION_ERROR;
+		if (close(executor->heredoc_fd) == -1)
+			exit_code = EXECUTION_ERROR;
+		executor->heredoc_fd_opened = false;
+	}
+	return (exit_code);
+}
+
 t_executor_exit_code	ms_executor_destroy(struct s_ms_executor *executor)
 {
+	t_executor_exit_code	exit_code;
+
 	executor->last_child_pid = -1;
 	vector_free(&executor->envp);
+	exit_code = EXECUTION_SUCCESS;
+	if (close(executor->stdin_fd) == -1)
+		exit_code = EXECUTION_ERROR;
 	if (ms_piper_destroy(&executor->piper))
-		return (EXECUTION_ERROR);
-	return (EXECUTION_SUCCESS);
+		exit_code = EXECUTION_ERROR;
+	return (exit_code);
 }
