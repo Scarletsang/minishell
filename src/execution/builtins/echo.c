e80fbf95448ca0f181bcb94817f1cfed14b9f7dc
Anthony Tsang <anthonytsang0226@gmail.com>
2023-06-24 03:55:03 +0200

    feat(execution/builtin): Add echo

--- src/execution/builtins/echo.c
+++ src/execution/builtins/echo.c
@@ -6,16 +6,30 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 19:21:02 by htsang            #+#    #+#             */
-/*   Updated: 2023/06/23 19:32:30 by htsang           ###   ########.fr       */
+/*   Updated: 2023/06/24 03:28:19 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "LIBFT/io.h"
 #include "MINISHELL/execution/builtins.h"
 
 int	ms_execute_builtin_echo(struct s_ms *ms, t_sb_vector *command)
 {
-	(void) command;
+	t_ft_vector_iterator	iterator;
+
 	(void) ms;
-	return (EXIT_SUCCESS);
+	ft_vector_iterator_init(&iterator, command);
+	ft_vector_iterator_next(&iterator);
+	while (!ft_vector_iterator_is_end(&iterator))
+	{
+		ft_putstr_fd((char *) \
+			((t_ft_sb *) ft_vector_iterator_current(&iterator))->buffer, \
+			STDOUT_FILENO);
+		ft_vector_iterator_next(&iterator);
+		if (!ft_vector_iterator_is_end(&iterator))
+			ft_putchar_fd(' ', STDOUT_FILENO);
+	}
+	ft_putchar_fd('\n', STDOUT_FILENO);
+	return (EC_SUCCESS);
 }
