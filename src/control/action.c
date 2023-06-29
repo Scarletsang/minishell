b83ad172db5c7665a3e4cc76f4894a7059f211eb
Anthony Tsang <anthonytsang0226@gmail.com>
2023-06-29 15:09:11 +0200

    chore(execution/vars): Change ms_vars_database_set function

--- src/control/action.c
+++ src/control/action.c
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 02:33:24 by htsang            #+#    #+#             */
-/*   Updated: 2023/06/26 02:38:54 by htsang           ###   ########.fr       */
+/*   Updated: 2023/06/29 00:09:51 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ int	ms_exit_code_save(struct s_ms *ms, t_ms_exit_code exit_code)
 	num = ft_itoa(exit_code);
 	if (!num)
 		return (EXIT_FAILURE);
-	if (ms_vars_database_set(&ms->vars.special, "?", num))
+	if (!ms_vars_database_set(&ms->vars.special, "?", num))
 	{
 		free(num);
 		return (EXIT_FAILURE);
