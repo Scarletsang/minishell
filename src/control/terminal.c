6ef6bcd8927bcd9e221b49224fd951c81e152e5c
Anthony Tsang <anthonytsang0226@gmail.com>
2023-06-29 17:13:32 +0200

    fix(control/terminal): disable echoing the signal characters is not implemented properly

--- src/control/terminal.c
+++ src/control/terminal.c
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/25 12:20:47 by htsang            #+#    #+#             */
-/*   Updated: 2023/06/26 02:52:10 by htsang           ###   ########.fr       */
+/*   Updated: 2023/06/29 15:56:09 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ms_terminal_settings_change(void)
 
 	if (tcgetattr(STDIN_FILENO, &term) == -1)
 		return (EXIT_FAILURE);
-	term.c_lflag |= ECHOCTL;
+	term.c_lflag &= ~ECHOCTL;
 	if (tcsetattr(STDIN_FILENO, TCSANOW, &term) == -1)
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
@@ -31,7 +31,7 @@ int	ms_terminal_settings_restore(void)
 
 	if (tcgetattr(STDIN_FILENO, &term) == -1)
 		return (EXIT_FAILURE);
-	term.c_lflag &= ~ECHOCTL;
+	term.c_lflag |= ECHOCTL;
 	if (tcsetattr(STDIN_FILENO, TCSANOW, &term) == -1)
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
