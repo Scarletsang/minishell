e1f3bec9ae0a268b5a0f1340570fccd227086afe
Sangyao42 <wangsangyao@hotmail.com>
2023-07-01 17:13:15 +0200

    Updated: env for not printing non-exported vars

--- src/execution/command/builtins/env.c
+++ src/execution/command/builtins/env.c
@@ -6,12 +6,13 @@
 /*   By: sawang <sawang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 19:21:02 by htsang            #+#    #+#             */
-/*   Updated: 2023/06/30 20:41:02 by sawang           ###   ########.fr       */
+/*   Updated: 2023/07/01 17:11:27 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "MINISHELL/execution/command/builtins.h"
+#include "LIBFT/string.h"
 #include <stdio.h>
 
 t_ms_exit_code	ms_execute_builtin_env(struct s_ms *ms, t_sb_vector *command)
@@ -24,7 +25,7 @@ t_ms_exit_code	ms_execute_builtin_env(struct s_ms *ms, t_sb_vector *command)
 	while (i < ms->vars.environment.capacity)
 	{
 		entry = ft_vector_get(&ms->vars.environment, i);
-		if (entry->key)
+		if (entry->key && ft_strchr(entry->value, '='))
 			printf("%s\n", (char *) entry->value);
 		i++;
 	}
