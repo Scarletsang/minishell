2a41ea7545cccf3f59a5847f1f3cb147b621d29d
Sangyao42 <wangsangyao@hotmail.com>
2023-07-03 14:31:05 +0200

    Updated: parameter name entry key to key

--- src/execution/command/builtins/unset.c
+++ src/execution/command/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 19:21:02 by htsang            #+#    #+#             */
-/*   Updated: 2023/07/01 13:41:13 by sawang           ###   ########.fr       */
+/*   Updated: 2023/07/03 14:00:24 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ t_ms_exit_code	ms_execute_builtin_unset(struct s_ms *ms, t_sb_vector *command)
 {
 	t_ms_exit_code				exit_code;
 	struct s_ft_vector_iterator	vec_iter;
-	char						*entry_key;
+	char						*key;
 
 	if (command->size == 1)
 		return (EC_SUCCESS);
@@ -41,16 +41,16 @@ t_ms_exit_code	ms_execute_builtin_unset(struct s_ms *ms, t_sb_vector *command)
 	ft_vector_iterator_next(&vec_iter);
 	while (!ft_vector_iterator_is_end(&vec_iter))
 	{
-		entry_key = ((t_ft_sb *)ft_vector_iterator_current(&vec_iter))->buffer;
-		if (!unset_name_is_valid_indentifier(entry_key))
+		key = ((t_ft_sb *)ft_vector_iterator_current(&vec_iter))->buffer;
+		if (!unset_name_is_valid_indentifier(key))
 		{
-			ms_error_printer_builtin("unset", entry_key,
+			ms_error_printer_builtin("unset", key,
 				"not a valid identifier");
 			exit_code = EC_FAILURE;
 			ft_vector_iterator_next(&vec_iter);
 			continue ;
 		}
-		ms_vars_unset(&ms->vars, entry_key);
+		ms_vars_unset(&ms->vars, key);
 		ft_vector_iterator_next(&vec_iter);
 	}
 	return (exit_code);
