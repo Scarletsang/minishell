8085fc63deffe56cbec7059b1b17596019bb6a19
Anthony Tsang <anthonytsang0226@gmail.com>
2023-07-07 05:11:03 +0200

    refactor(builtin): unset is now using t_ft_str instead of constructing a key using malloc

--- src/execution/command/builtins/unset.c
+++ src/execution/command/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 19:21:02 by htsang            #+#    #+#             */
-/*   Updated: 2023/07/07 01:22:41 by htsang           ###   ########.fr       */
+/*   Updated: 2023/07/07 04:21:23 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,25 +34,27 @@ t_ms_exit_code	ms_execute_builtin_unset(struct s_ms *ms, t_sb_vector *command)
 {
 	t_ms_exit_code				exit_code;
 	struct s_ft_vector_iterator	vec_iter;
-	char						*key;
+	t_ft_str_nt					key;
 
 	if (command->size == 1)
 		return (EC_SUCCESS);
 	exit_code = EC_SUCCESS;
 	ft_vector_iterator_begin(&vec_iter, command);
-	ft_vector_iterator_next(&vec_iter);
-	while (!vec_iter.is_end)
+	while (!ft_vector_iterator_next(&vec_iter))
 	{
-		key = ((t_ft_sb *)ft_vector_iterator_current(&vec_iter))->buffer;
-		if (!name_is_valid_indentifier(key, '\0'))
+		key = (t_ft_str_nt){
+			.content = \
+				((t_ft_sb *) ft_vector_iterator_current(&vec_iter))->buffer,
+			.size = ((t_ft_sb *)ft_vector_iterator_current(&vec_iter))->size
+		};
+		if (!name_is_valid_indentifier(key.content, '\0'))
 		{
-			ms_error_printer_builtin("unset", key,
+			ms_error_printer_builtin("unset", key.content,
 				"not a valid identifier");
 			exit_code = EC_FAILURE;
 		}
 		else
 			ms_vars_unset(&ms->vars, key);
-		ft_vector_iterator_next(&vec_iter);
 	}
 	return (exit_code);
 }
